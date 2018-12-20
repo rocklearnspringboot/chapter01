61e170d7b95e4d19e425bede25710f20e9f9d1c9
rocklearnspringboot <learnspringboot@126.com>
2018-12-20 23:11:02 +0800

    start springboot

--- src/main/java/com/springboot/chapter01/main/Chapter01Main.java
+++ src/main/java/com/springboot/chapter01/main/Chapter01Main.java
@@ -1,4 +1,27 @@
 package com.springboot.chapter01.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
 public class Chapter01Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter01Main.class, args);
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public Map<String, String> test() {
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        return map;
+    }
 }
